5c429f6f2b8af99c9432cba22ec6b099ae6c1aef
Shekhar Dangi <arpitt682@gmail.com>
2025-07-22 18:05:36 +0530

    chore: setup server

---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response, Application } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/health", (req: Request, res: Response) => {
+  res.send("server is healthy!");
+});
+
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 4000;
+
+app
+  .listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  })
+  .on("error", (err: any) => {
+    if (err.code === "EADDRINUSE") {
+      console.log("Error: address already in use");
+    } else {
+      console.log(err);
+    }
+  });
